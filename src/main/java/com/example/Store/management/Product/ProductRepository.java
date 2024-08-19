18269f7ef5417e1e97171595cbbef29da16e0668
amir1382-nas <132781190+amir1382-nas@users.noreply.github.com>
2024-08-19 12:21:29 +0330

    Store Management

--- src/main/java/com/example/Store/management/Product/ProductRepository.java
+++ src/main/java/com/example/Store/management/Product/ProductRepository.java
@@ -6,9 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    void addProduct(Product product);
-//    void updateProduct(Product product);
-//    void deleteProduct(int productId);
-//    List<Product> getAllProducts();
-//    Product getProduct(int productId);
 }
