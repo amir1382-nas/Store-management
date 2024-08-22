7c195697a8028c86a0e81410dd20aa60e0376aac
amir1382-nas <132781190+amir1382-nas@users.noreply.github.com>
2024-08-22 14:02:48 +0330

    Store Management

---
+++ src/main/java/com/example/Store/management/JWT/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.example.Store.management.JWT;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/Store/management/JWT/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.example.Store.management.JWT;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
